2d1fe1403d0d6dc641b86f18528ccfd8dfd25152
TashiJoisher <84910358+TashiJoisher@users.noreply.github.com>
2022-03-23 13:35:58 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,14 +1,15 @@
 const firebaseConfig = {
-      apiKey: "AIzaSyCBbN7V6JIZqBObXeWL3vr0W3WR5_8Vs5U",
-      authDomain: "coding-78a2f.firebaseapp.com",
-      databaseURL: "https://coding-78a2f-default-rtdb.firebaseio.com",
-      projectId: "coding-78a2f",
-      storageBucket: "coding-78a2f.appspot.com",
-      messagingSenderId: "272428798203",
-      appId: "1:272428798203:web:d63444e2526191ab01605e"
-    };
-    // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCBbN7V6JIZqBObXeWL3vr0W3WR5_8Vs5U",
+  authDomain: "coding-78a2f.firebaseapp.com",
+  databaseURL: "https://coding-78a2f-default-rtdb.firebaseio.com",
+  projectId: "coding-78a2f",
+  storageBucket: "coding-78a2f.appspot.com",
+  messagingSenderId: "272428798203",
+  appId: "1:272428798203:web:d63444e2526191ab01605e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
       user_name = localStorage.getItem("user_name");
     
